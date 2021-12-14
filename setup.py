77a9e1573209c17ac8d72d911481dc03b06a5cfe
Joao Paulo Reis Alvarenga <joaopaulo.reisalvarenga@gmail.com>
2021-12-13 22:46:31 -0300

    fix: add requirements and workflows

--- setup.py
+++ setup.py
@@ -12,6 +12,7 @@ setuptools.setup(
     version='0.0.1',
     package_dir={"": "src"},
     packages=setuptools.find_packages(where="src"),
+    install_requires=requirements,
     python_requires=">=3.6",
     entry_points={'console_scripts': ['voice_assistant = voice_assistant_.__main__:main']},
 )
